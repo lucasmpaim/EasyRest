cd279f812c0c0e79148aab11c21a39d1d688a4ae
Lucas Paim <lucasmpaim1@gmail.com>
2016-08-29 11:32:14 -0300

    add upload func to service

--- Pod/Classes/Service.swift
+++ Pod/Classes/Service.swift
@@ -19,11 +19,27 @@ public class Service<R: Routable> {
         }
     }
     
-    public func builder<T: MappableBase>(routes: R, type: T.Type) throws -> APIBuilder<T> {
+    public func builder<T: MappableBase>(routes: R,
+                                         type: T.Type) throws -> APIBuilder<T> {
         return try routes.builder(base, type: type)
     }
     
-    public func call<E: MappableBase>(routes: R, type: E.Type, onSuccess: (result: E?) -> Void, onError: (RestError?) -> Void, always: () -> Void) throws {
+    public func call<E: MappableBase>(routes: R,
+                                      type: E.Type,
+                                      onSuccess: (result: E?) -> Void,
+                                      onError: (RestError?) -> Void,
+                                      always: () -> Void) throws {
         try builder(routes, type: type).build().execute(onSuccess, onError: onError, always: always)
+    }
+
+    public func upload<E: MappableBase>(routes: R, type: E.Type,
+                                        onSuccess: (result: E?) -> Void,
+                                        onProgress: (progress: Float) -> Void,
+                                        onError: (RestError?) -> Void,
+                                        always: () -> Void) throws {
+        try builder(routes, type: type).build().upload(onSuccess,
+                onProgress: onProgress,
+                onError: onError,
+                always: always)
     }
 }
