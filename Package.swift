d20b75d655a03f2f0fb59309196138f0e7f183cc
Ráfagan Abreu <rafagansa@outlook.com>
2020-09-29 17:58:31 -0300

    fix: alamofire SPM

--- Package.swift
+++ Package.swift
@@ -13,7 +13,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(
+            url: "https://github.com/Alamofire/Alamofire.git", 
+            from: "4.8.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
